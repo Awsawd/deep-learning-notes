3a6907829388c55791a50d75d67fcf9ef6d7c26c
suez <2465239507@qq.com>
2026-07-15 20:22:31 +0800

    LeNet

--- Classify Leaves/scripts/00_eda.py
+++ Classify Leaves/scripts/00_eda.py
@@ -6,9 +6,11 @@ from pathlib import Path
 PROJECT_PATH = Path(__file__).resolve().parent.parent
 df = pd.read_csv(PROJECT_PATH / 'data' / 'classify-leaves' / 'train.csv')
 
-print(df['label'].value_counts())
-print("--------------------------------")
-#看看每类样本的图片数量
-for label in df['label'].unique():
-    print(f"Label {label}: {len(df[df['label'] == label])} images")
-print("--------------------------------")
+# print(df['label'].value_counts())
+# print("--------------------------------")
+# #看看每类样本的图片数量
+# for label in df['label'].unique():
+#     print(f"Label {label}: {len(df[df['label'] == label])} images")
+# print("--------------------------------")
+print(df['image'].tolist())
+
